6cabbb24a750f6cc9d21211362f63b284b2063fa
Edivania <edivania.smaximino@senacsp.edu.br>
2025-12-24 13:46:34 -0300

    animação menu-toggle

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,9 @@
-const toggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.nav');
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('.nav');
 
-toggle.addEventListener('click', () => {
-  nav.classList.toggle('active');
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    toggle.classList.toggle('active');
+  });
 });
